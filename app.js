399d5dff7ef836cc519ef70a9dd0a7f0012b8e7e
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-03-13 15:04:39 +0100

    code added

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const svgCircle = document.getElementById('svgCircle')
-const stepOne = document.getElementById('step-one')
-const stepTwo = document.getElementById('step-two')
-const stepThree = document.getElementById('step-three')
-const stepFour = document.getElementById('step-four')
-const stepFive = document.getElementById('step-five')
+const svgCircle = document.getElementById('svg-circle');
+const stepOne = document.getElementById('step-one');
+const stepTwo = document.getElementById('step-two');
+const stepThree = document.getElementById('step-three');
+const stepFour = document.getElementById('step-four');
+const stepFive = document.getElementById('step-five');
+
+stepOne.addEventListener('click', () => {
+	svgCircle.style.strokeDashoffset = '1004';
+});
+
+stepTwo.addEventListener('click', () => {
+	svgCircle.style.strokeDashoffset = '753';
+    
+});
+
+stepThree.addEventListener('click', () => {
+	svgCircle.style.strokeDashoffset = '502';
+    
+});
+
+stepFour.addEventListener('click', () => {
+	svgCircle.style.strokeDashoffset = '251';
+});
+
+stepFive.addEventListener('click', () => {
+	svgCircle.style.strokeDashoffset = '0';
+});
