03c3e455101cc44be9723ea06eebcabd6186ab60
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-03-13 15:10:34 +0100

    code added

--- app.js
+++ app.js
@@ -4,25 +4,31 @@ const stepTwo = document.getElementById('step-two');
 const stepThree = document.getElementById('step-three');
 const stepFour = document.getElementById('step-four');
 const stepFive = document.getElementById('step-five');
+const main = document.getElementById('main');
+
+main.style.backgroundImage = 'url(img/img-1.jpg)';
 
 stepOne.addEventListener('click', () => {
 	svgCircle.style.strokeDashoffset = '1004';
+    main.style.backgroundImage = 'url(img/img-2.jpg)';
 });
 
 stepTwo.addEventListener('click', () => {
 	svgCircle.style.strokeDashoffset = '753';
-    
+    main.style.backgroundImage = 'url(img/img-3.jpg)';
 });
 
 stepThree.addEventListener('click', () => {
 	svgCircle.style.strokeDashoffset = '502';
-    
+    main.style.backgroundImage = 'url(img/img-4.jpg)';
 });
 
 stepFour.addEventListener('click', () => {
 	svgCircle.style.strokeDashoffset = '251';
+    main.style.backgroundImage = 'url(img/img-5.jpg)';
 });
 
 stepFive.addEventListener('click', () => {
 	svgCircle.style.strokeDashoffset = '0';
+    main.style.backgroundImage = 'url(img/img-6.jpg)';
 });
